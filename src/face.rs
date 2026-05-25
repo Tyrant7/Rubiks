f4a2cd6c1fde7d864403723a1cbf7b373ce104c3
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-05-24 17:07:46 -0700

    Restored accidentally deleted tests

--- src/face.rs
+++ src/face.rs
@@ -199,3 +199,100 @@ impl FaceType {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_face() {
+        let face = Face::new(Colour::Green);
+        for i in 0..CUBE_SIZE {
+            for j in 0..CUBE_SIZE {
+                assert_eq!(face.get_tile_colour(i, j), Colour::Green);
+            }
+        }
+    }
+
+    #[test]
+    fn make_turn_clockwise() {
+        let mut face = Face::new(Colour::Blue);
+        face.set_tile_colour(0, 0, Colour::Green);
+        face.set_tile_colour(0, 1, Colour::White);
+        face.set_tile_colour(2, 2, Colour::Red);
+        // G W B
+        // B B B
+        // B B R
+
+        face.make_turn(TurnType::Clockwise);
+        // B B G
+        // B B W
+        // R B B
+
+        let mut target_face = Face::new(Colour::Blue);
+        target_face.set_tile_colour(0, 2, Colour::Green);
+        target_face.set_tile_colour(1, 2, Colour::White);
+        target_face.set_tile_colour(2, 0, Colour::Red);
+
+        assert_eq!(face, target_face);
+    }
+
+    #[test]
+    fn make_turn_counterclockwise() {
+        let mut face = Face::new(Colour::Blue);
+        face.set_tile_colour(0, 0, Colour::Green);
+        face.set_tile_colour(0, 1, Colour::White);
+        face.set_tile_colour(2, 2, Colour::Red);
+        // G W B
+        // B B B
+        // B B R
+
+        face.make_turn(TurnType::CounterClockwise);
+        // B B R
+        // W B B
+        // G B B
+
+        let mut target_face = Face::new(Colour::Blue);
+        target_face.set_tile_colour(0, 2, Colour::Red);
+        target_face.set_tile_colour(1, 0, Colour::White);
+        target_face.set_tile_colour(2, 0, Colour::Green);
+
+        assert_eq!(face, target_face);
+    }
+
+    #[test]
+    fn make_half_turn() {
+        let mut face = Face::new(Colour::Blue);
+        face.set_tile_colour(0, 0, Colour::Green);
+        face.set_tile_colour(0, 1, Colour::White);
+        face.set_tile_colour(2, 2, Colour::Red);
+        // G W B
+        // B B B
+        // B B R
+
+        face.make_turn(TurnType::Half);
+        // R B B
+        // B B B
+        // B W G
+
+        let mut target_face = Face::new(Colour::Blue);
+        target_face.set_tile_colour(0, 0, Colour::Red);
+        target_face.set_tile_colour(2, 1, Colour::White);
+        target_face.set_tile_colour(2, 2, Colour::Green);
+
+        assert_eq!(face, target_face);
+    }
+
+    #[test]
+    fn is_solved_false() {
+        let mut face = Face::new(Colour::Yellow);
+        face.set_tile_colour(0, 0, Colour::Blue);
+        assert!(!face.is_solved());
+    }
+
+    #[test]
+    fn is_solved_true() {
+        let face = Face::new(Colour::Yellow);
+        assert!(face.is_solved());
+    }
+}
