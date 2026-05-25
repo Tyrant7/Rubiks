06de37998c5121235f3f718e97c5286a7a3ee989
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-05-24 18:18:06 -0700

    Torch cpu working

--- rl/src/main.rs
+++ rl/src/main.rs
@@ -1,3 +1,11 @@
+use rubiks::Cube;
+
 fn main() {
-    println!("Hello, world!");
+    let cube = Cube::default();
+    println!("{:#?}", cube);
+
+    // Hooray!
+    let t = tch::Tensor::from_slice(&[3, 1, 4, 1, 5]);
+    let t = t * 2;
+    t.print();
 }
