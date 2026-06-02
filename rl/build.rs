c3df1762e4664ebec73c565dc42e30950d5e93a4
Tyler Sauve <tyler.sauve@bag-of-holding.com>
2026-06-02 15:20:04 -0700

    Added explanation comment

--- rl/build.rs
+++ rl/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    // Force the linker to include the CUDA warp_size symbol which would
+    // otherwise be dropped by the Windows linker, causing CUDA to silently
+    // fail to initialize at runtime.
     println!("cargo:rustc-link-arg=/INCLUDE:?warp_size@cuda@at@@YAHXZ");
 }
